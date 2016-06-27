cf8d778804f8993da1c447065155918b7309a27f
stirringhalo <stirringhalo@users.noreply.github.com>
2016-06-27 10:32:13 -0400

    Update mdhs.sh

--- demo_code/mdhs.sh
+++ demo_code/mdhs.sh
@@ -1,5 +1,6 @@
 # https://gis.stackexchange.com/questions/146296/how-to-create-composite-hillshade
-
+# Give the input DEM.
+# When combined with the combined_hillshade.py script, the output hillshade is really better to show texture. Can tune the formula in combined_hillshade.py to stretch the colors better but illuminating from all angles loses the sense of topography
 gdaldem hillshade $1 hillshades_0.tmp.tif -s 111120 -z 3 -az 0 -alt 45 -compute_edges
 gdaldem hillshade $1 hillshades_45.tmp.tif -s 111120 -z 3 -az 45 -alt 45 -compute_edges
 gdaldem hillshade $1 hillshades_90.tmp.tif -s 111120 -z 3 -az 90 -alt 45 -compute_edges
